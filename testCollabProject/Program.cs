3f6abf0dd365246bd1e1a6f53bdc5d92056f254b
SethKaminski <sethgabe@yahoo.com>
2016-10-18 08:18:14 -0400

    Conflict

--- testCollabProject/Program.cs
+++ testCollabProject/Program.cs
@@ -10,7 +10,7 @@ namespace testCollabProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World ");
+            Console.WriteLine("World Hello");
         }
     }
 }
